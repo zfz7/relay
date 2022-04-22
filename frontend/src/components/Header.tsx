39b03287883d5613778c74957312b94b929337ed
Daniel Eichman <D.Eichman@outlook.com>
2022-04-22 15:11:36 -0700

    fix margin

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -9,7 +9,7 @@ export const Header: React.FC = () => {
       position="sticky"
       elevation={0}
     >
-      <Grid container alignItems="center" style={{margin:"4px"}}>
+      <Grid container alignItems="center" style={{margin:"4px 0"}}>
         <Grid item>
           <IconButton size={"small"} edge="start" style={{marginLeft: "0.5rem"}}>
             <img src={logo} alt="Logo" width="40px"/>
