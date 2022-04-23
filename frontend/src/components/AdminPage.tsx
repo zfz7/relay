c76872d43d96fad4b8e67fa8c4cb8219aacd9443
Daniel Eichman <D.Eichman@outlook.com>
2022-04-22 21:41:03 -0700

    Admin page linting

--- frontend/src/components/AdminPage.tsx
+++ frontend/src/components/AdminPage.tsx
@@ -25,56 +25,56 @@ export const AdminPage: React.FC = () => {
   return (<>
       <Header/>
       <Container>
-      <Grid sx={{mt: '1rem'}} container direction="row" spacing={2}>
-        <Grid item xs={9}>
-          <TableContainer component={Paper}>
-            <Table data-testid="peerTable">
-              <TableHead>
-                <TableRow>
-                  <TableCell align="left">Address</TableCell>
-                  <TableCell align="right">Expiration</TableCell>
-                </TableRow>
-              </TableHead>
-              {peers && <TableBody>
-                {peers.peers.sort((a, b) => a.expiration.getTime() - b.expiration.getTime()).map((peer) => (
-                  <TableRow
-                    key={peer.address}
-                  >
-                    <TableCell align="left">{peer.address}</TableCell>
-                    <TableCell align="right">{peer.expiration.toDateString()}</TableCell>
+        <Grid sx={{mt: '1rem'}} container direction="row" spacing={2}>
+          <Grid item xs={9}>
+            <TableContainer component={Paper}>
+              <Table data-testid="peerTable">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="left">Address</TableCell>
+                    <TableCell align="right">Expiration</TableCell>
                   </TableRow>
-                ))}
-              </TableBody>}
-            </Table>
-          </TableContainer>
+                </TableHead>
+                {peers && <TableBody>
+                  {peers.peers.sort((a, b) => a.expiration.getTime() - b.expiration.getTime()).map((peer) => (
+                    <TableRow
+                      key={peer.address}
+                    >
+                      <TableCell align="left">{peer.address}</TableCell>
+                      <TableCell align="right">{peer.expiration.toDateString()}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>}
+              </Table>
+            </TableContainer>
+          </Grid>
+          <Grid item xs={3}>
+            <Card sx={{backgroundColor: theme.palette.primary.light}} raised data-testid="activeUsers">
+              <CardContent>
+                <Typography>Active users</Typography>
+                <Typography variant="h3">{peers?.peers.length}</Typography>
+              </CardContent>
+            </Card>
+            <Card sx={{backgroundColor: theme.palette.primary.light, mt: '1rem'}} raised data-testid="invalidAdmin">
+              <CardContent>
+                <Typography>Invalid Admin Attempts</Typography>
+                <Typography variant="h3">{logs?.invalidAdminAccessEvents.length}</Typography>
+              </CardContent>
+            </Card>
+            <Card sx={{backgroundColor: theme.palette.primary.light, mt: '1rem'}} raised data-testid="invalidCode">
+              <CardContent>
+                <Typography>Invalid Access Code Attempts</Typography>
+                <Typography variant="h3">{logs?.invalidAccessCodeEvents.length}</Typography>
+              </CardContent>
+            </Card>
+            <Card sx={{backgroundColor: theme.palette.primary.light, mt: '1rem'}} raised data-testid="removedPeers">
+              <CardContent>
+                <Typography>Removed Peers</Typography>
+                <Typography variant="h3">{logs?.peerRemovedEvents.length}</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
         </Grid>
-        <Grid item xs={3}>
-          <Card sx={{backgroundColor: theme.palette.primary.light}} raised data-testid="activeUsers">
-            <CardContent>
-              <Typography>Active users</Typography>
-              <Typography variant="h3">{peers?.peers.length}</Typography>
-            </CardContent>
-          </Card>
-          <Card sx={{backgroundColor: theme.palette.primary.light, mt: '1rem'}} raised data-testid="invalidAdmin">
-            <CardContent>
-              <Typography>Invalid Admin Attempts</Typography>
-              <Typography variant="h3">{logs?.invalidAdminAccessEvents.length}</Typography>
-            </CardContent>
-          </Card>
-          <Card sx={{backgroundColor: theme.palette.primary.light, mt: '1rem'}} raised data-testid="invalidCode">
-            <CardContent>
-              <Typography>Invalid Access Code Attempts</Typography>
-              <Typography variant="h3">{logs?.invalidAccessCodeEvents.length}</Typography>
-            </CardContent>
-          </Card>
-          <Card sx={{backgroundColor: theme.palette.primary.light, mt: '1rem'}} raised data-testid="removedPeers">
-            <CardContent>
-              <Typography>Removed Peers</Typography>
-              <Typography variant="h3">{logs?.peerRemovedEvents.length}</Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
       </Container>
     </>
   )
