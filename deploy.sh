87de922978aaa5af3552493b44eae40812429b05
Daniel Eichman <D.Eichman@outlook.com>
2022-04-24 09:49:10 -0700

    Variablize deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,20 @@ set -e
 git update-index --refresh
 git diff-index --quiet HEAD --
 
+sshHost="-p $RELAY_SSH_PORT -C $RELAY_SSH_USER@$RELAY_URL"
+scpHost="$RELAY_SSH_USER@$RELAY_URL"
+
+docker-compose up -d
 ./gradlew clean test build
 
-ssh relay "sudo killall java" || echo "No Java process running"
-ssh relay "sudo rm ~/app/relay.jar" || echo "No Java process running"
-ssh relay "docker-compose -f ~/app/docker-compose.prod.yml down" || echo "Docker not running"
-scp ./build/libs/relay.jar relay:~/app/
-scp ./docker-compose.prod.yml relay:~/app/
-ssh relay "docker-compose -f ~/app/docker-compose.prod.yml up -d"
-ssh relay "docker-compose -f ~/app/docker-compose.prod.yml restart wireguard"
-ssh relay "cd ~/app && sudo java -jar ./relay.jar --spring.profiles.active=cloud \
+ssh $sshHost "sudo killall java" || echo "No Java process running"
+ssh $sshHost "sudo rm ~/app/relay.jar" || echo "No Java process running"
+ssh $sshHost "docker-compose -f ~/app/docker-compose.prod.yml down" || echo "Docker not running"
+scp -P "$RELAY_SSH_PORT" ./build/libs/relay.jar $scpHost:~/app/
+scp -P "$RELAY_SSH_PORT" ./docker-compose.prod.yml $scpHost:~/app/
+ssh $sshHost "docker-compose -f ~/app/docker-compose.prod.yml up -d"
+ssh $sshHost "docker-compose -f ~/app/docker-compose.prod.yml restart wireguard"
+ssh $sshHost "cd ~/app && sudo java -jar ./relay.jar --spring.profiles.active=cloud \
         --WG_CODE=${WG_CODE} \
         --GITHUB_PROD_CLIENT_ID=${GITHUB_PROD_CLIENT_ID} \
         --GITHUB_PROD_CLIENT_SECRET=${GITHUB_PROD_CLIENT_SECRET} \
