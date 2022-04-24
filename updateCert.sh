fbc6804f32b3c974f3001f2477f8cd57aedd9374
Daniel Eichman <D.Eichman@outlook.com>
2022-04-24 09:50:23 -0700

    Variablize updateCert.sh

--- updateCert.sh
+++ updateCert.sh
@@ -2,16 +2,18 @@
 
 set -e
 
+sshHost="-p $RELAY_SSH_PORT -C $RELAY_SSH_USER@$RELAY_URL"
+
 git update-index --refresh
 git diff-index --quiet HEAD --
 
-ssh relay "sudo certbot certificates"
-ssh relay "sudo certbot certonly --standalone -d relay.zfz7.org -n"
-ssh relay "sudo openssl pkcs12 -export -in /etc/letsencrypt/live/relay.zfz7.org/fullchain.pem -inkey /etc/letsencrypt/live/relay.zfz7.org/privkey.pem -out /etc/letsencrypt/live/relay.zfz7.org/keystore.p12 -name tomcat -CAfile /etc/letsencrypt/live/relay.zfz7.org/chain.pem -caname root -passout pass:relay"
-ssh relay "sudo killall java" || echo "No Java process running"
-ssh relay "docker-compose -f ~/app/docker-compose.prod.yml up -d"
-ssh relay "docker-compose -f ~/app/docker-compose.prod.yml restart wireguard"
-ssh relay "cd ~/app && sudo java -jar ./relay.jar --spring.profiles.active=cloud \
+ssh $sshHost "sudo certbot certificates"
+ssh $sshHost "sudo certbot certonly --standalone -d relay.zfz7.org -n"
+ssh $sshHost "sudo openssl pkcs12 -export -in /etc/letsencrypt/live/relay.zfz7.org/fullchain.pem -inkey /etc/letsencrypt/live/relay.zfz7.org/privkey.pem -out /etc/letsencrypt/live/relay.zfz7.org/keystore.p12 -name tomcat -CAfile /etc/letsencrypt/live/relay.zfz7.org/chain.pem -caname root -passout pass:relay"
+ssh $sshHost "sudo killall java" || echo "No Java process running"
+ssh $sshHost "docker-compose -f ~/app/docker-compose.prod.yml up -d"
+ssh $sshHost "docker-compose -f ~/app/docker-compose.prod.yml restart wireguard"
+ssh $sshHost "cd ~/app && sudo java -jar ./relay.jar --spring.profiles.active=cloud \
         --WG_CODE=${WG_CODE} \
         --GITHUB_PROD_CLIENT_ID=${GITHUB_PROD_CLIENT_ID} \
         --GITHUB_PROD_CLIENT_SECRET=${GITHUB_PROD_CLIENT_SECRET} \
