c411d608920ef8d916ee300ebc05a0254a19944f
Daniel Eichman <D.Eichman@outlook.com>
2022-04-29 16:17:43 -0700

    Use SSL_KEY_STORE_PASSWORD for cert pw

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ else
 fi
 
 ssh $sshHost "mkdir -p ~/app/config"
-ssh $sshHost "sudo openssl pkcs12 -export -in /etc/letsencrypt/live/$RELAY_URL/fullchain.pem -inkey /etc/letsencrypt/live/$RELAY_URL/privkey.pem -out /etc/letsencrypt/live/$RELAY_URL/keystore.p12 -name tomcat -CAfile /etc/letsencrypt/live/$RELAY_URL/chain.pem -caname root -passout pass:relay"
+ssh $sshHost "sudo openssl pkcs12 -export -in /etc/letsencrypt/live/$RELAY_URL/fullchain.pem -inkey /etc/letsencrypt/live/$RELAY_URL/privkey.pem -out /etc/letsencrypt/live/$RELAY_URL/keystore.p12 -name tomcat -CAfile /etc/letsencrypt/live/$RELAY_URL/chain.pem -caname root -passout pass:$SSL_KEY_STORE_PASSWORD"
 
 ssh $sshHost "sudo killall java" || echo "No Java process running"
 ssh $sshHost "sudo rm ~/app/relay.jar" || echo "No Java process running"
@@ -31,4 +31,5 @@ ssh $sshHost "cd ~/app && sudo java -jar ./relay.jar --spring.profiles.active=cl
         --RELAY_WG_PORT=${RELAY_WG_PORT} \
         --GITHUB_PROD_CLIENT_ID=${GITHUB_PROD_CLIENT_ID} \
         --GITHUB_PROD_CLIENT_SECRET=${GITHUB_PROD_CLIENT_SECRET} \
+        --SSL_KEY_STORE_PASSWORD=${SSL_KEY_STORE_PASSWORD} \
         --POSTGRES_DB_PASSWORD=${POSTGRES_DB_PASSWORD} &"
--- updateCert.sh
+++ updateCert.sh
@@ -9,7 +9,7 @@ git diff-index --quiet HEAD --
 
 ssh $sshHost "sudo certbot certificates"
 ssh $sshHost "sudo certbot certonly --standalone -d $RELAY_URL -n"
-ssh $sshHost "sudo openssl pkcs12 -export -in /etc/letsencrypt/live/$RELAY_URL/fullchain.pem -inkey /etc/letsencrypt/live/$RELAY_URL/privkey.pem -out /etc/letsencrypt/live/$RELAY_URL/keystore.p12 -name tomcat -CAfile /etc/letsencrypt/live/$RELAY_URL/chain.pem -caname root -passout pass:relay"
+ssh $sshHost "sudo openssl pkcs12 -export -in /etc/letsencrypt/live/$RELAY_URL/fullchain.pem -inkey /etc/letsencrypt/live/$RELAY_URL/privkey.pem -out /etc/letsencrypt/live/$RELAY_URL/keystore.p12 -name tomcat -CAfile /etc/letsencrypt/live/$RELAY_URL/chain.pem -caname root -passout pass:$SSL_KEY_STORE_PASSWORD"
 ssh $sshHost "sudo killall java" || echo "No Java process running"
 ssh $sshHost "RELAY_URL=${RELAY_URL} POSTGRES_DB_PASSWORD=${POSTGRES_DB_PASSWORD} RELAY_WG_PORT=${RELAY_WG_PORT} docker compose -f ~/app/docker-compose.prod.yml up -d"
 ssh $sshHost "RELAY_URL=${RELAY_URL} POSTGRES_DB_PASSWORD=${POSTGRES_DB_PASSWORD} RELAY_WG_PORT=${RELAY_WG_PORT} docker compose -f ~/app/docker-compose.prod.yml restart wireguard"
@@ -18,4 +18,5 @@ ssh $sshHost "cd ~/app && sudo java -jar ./relay.jar --spring.profiles.active=cl
         --RELAY_WG_PORT=${RELAY_WG_PORT} \
         --GITHUB_PROD_CLIENT_ID=${GITHUB_PROD_CLIENT_ID} \
         --GITHUB_PROD_CLIENT_SECRET=${GITHUB_PROD_CLIENT_SECRET} \
+        --SSL_KEY_STORE_PASSWORD=${SSL_KEY_STORE_PASSWORD} \
         --POSTGRES_DB_PASSWORD=${POSTGRES_DB_PASSWORD} &"
