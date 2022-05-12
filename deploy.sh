cdec5cf1a9ad0c86c2070305efe02728fb292044
Daniel Eichman <D.Eichman@outlook.com>
2022-05-12 08:16:15 -0700

    Update system on deploy

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,25 @@ git diff-index --quiet HEAD --
 sshHost="-p $RELAY_SSH_PORT -C $RELAY_SSH_USER@$RELAY_URL"
 scpHost="$RELAY_SSH_USER@$RELAY_URL"
 
+#Update system
+ssh $sshHost "sudo apt update"
+ssh $sshHost "sudo apt upgrade -y"
+ssh $sshHost "sudo apt autoremove -y"
+ssh $sshHost "sudo killall java" || echo "No Java process running"
+ssh $sshHost "sudo shutdown -r now"
+
+##Build App
 if [ "$1" == 'NOTEST' ]; then
 	./gradlew clean build -x test
 else
 	./gradlew clean test build
 fi
+
+#Wait for host to come back online
+until [ "$(ssh $sshHost "echo ok")" = "ok" ]; do
+  sleep 1;
+  echo "Trying to connect to host again..."
+done
 
 ssh $sshHost "mkdir -p ~/app/config"
 ssh $sshHost "sudo openssl pkcs12 -export -in /etc/letsencrypt/live/$RELAY_URL/fullchain.pem -inkey /etc/letsencrypt/live/$RELAY_URL/privkey.pem -out /etc/letsencrypt/live/$RELAY_URL/keystore.p12 -name tomcat -CAfile /etc/letsencrypt/live/$RELAY_URL/chain.pem -caname root -passout pass:$SSL_KEY_STORE_PASSWORD"
