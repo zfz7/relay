35397b8fbe86ed221b2194e0cfd7042e79cede75
Daniel Eichman <d.eichman@outlook.com>
2022-07-10 15:26:58 -0700

    Clean up gradle file

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -26,8 +26,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
